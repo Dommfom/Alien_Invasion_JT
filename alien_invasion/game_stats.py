75fbb8ca48fd9f7bf0c320074c5e04e44fcb75e8
Dommfom <81488990+Dommfom@users.noreply.github.com>
2021-09-23 19:26:34 +0100

    Add files via upload

---
+++ alien_invasion/game_stats.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 22 12:14:55 2021
+
+@author: jason
+"""
+
+class GameStats:
+    """Track stats of the game."""
+
+    def __init__(self, ai_game):
+        """initialise"""
+        self.settings = ai_game.settings
+        self.reset_stats()
+        # Start Alien Invasion in an active state.
+        self.game_active = False
+        self.hiscore = 0
+    
+    def reset_stats(self):
+        """Initalise stats that can change during the game"""
+        self.ships_left = self.settings.ship_limit
+        self.score = 0
